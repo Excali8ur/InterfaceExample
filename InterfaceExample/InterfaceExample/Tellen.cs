035e97adcb66985d9fd32e7c55389575ffd7bb2f
Rianne Boumans <rianne.boumans@zuyd.nl>
2018-09-18 14:50:29 +0200

    Inital project

---
+++ InterfaceExample/InterfaceExample/Tellen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExample
+{
+    public class Tellen : iTellen
+    {
+        public int Optellen(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Aftrekken(int x, int y)
+        {
+            return x - y;
+        }
+
+        public float Vermenigvuldigen(int x, int y)
+        {
+            return x * y;
+        }
+
+        public float Delen(int x, int y)
+        {
+            if (y == 0)
+            {
+                throw new DivideByZeroException("Delen door nul is flauwekul");
+            }
+            else
+            {
+                return x / y;
+            }
+        }
+    }
+}
---
+++ InterfaceExample/InterfaceExample/iTellen.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExample
+{
+    public interface iTellen
+    {
+        /// <summary>
+        /// Telt 2 getallen bij elkaar op en retouneert het antwoord
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>Resultaat van berekening</returns>
+        int Optellen(int x, int y);
+        
+        /// <summary>
+        /// Trekt 2 getallen van elkaar af en retouneert het antwoord
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>Resultaat van berekening</returns>
+        int Aftrekken(int x, int y);
+
+        /// <summary>
+        /// Vermenigvuldigd van 2 getallen en retouneert het antwoord
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>Resultaat van berekening</returns>
+        float Vermenigvuldigen(int x, int y);
+
+        /// <summary>
+        /// Deelt 2 getallen en retouneert het antwoord
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>Resultaat van berekening</returns>
+        float Delen(int x, int y);
+
+    }
+}
